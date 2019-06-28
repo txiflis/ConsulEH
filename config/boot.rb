9fb85ea8b2e6d2a281455e94bf55e82df17ffde2
txiflis <txiflis@hotmail.com>
2019-06-28 13:42:58 +0200

    lander_072

--- config/boot.rb
+++ config/boot.rb
@@ -1,16 +1,5 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
-<<<<<<< HEAD
-require 'bundler/setup' # Set up gems listed in the Gemfile.
 
-require 'rails/commands/server'
-module Rails
-  class Server
-    def default_options
-      super.merge(Host:  '0.0.0.0', Port: 3000)
-    end
-  end
-end
-=======
 require "bundler/setup" # Set up gems listed in the Gemfile.
->>>>>>> upstream/capistrano
+
